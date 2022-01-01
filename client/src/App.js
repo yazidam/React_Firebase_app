f8a690838ebfed4f86bf01939255993747e7a388
@ahmedyazid.mejri <ahmedyazid.mejri@esprit.tn>
2022-01-01 16:18:54 +0100

    Creating post and adding to firebase

--- client/src/App.js
+++ client/src/App.js
@@ -16,23 +16,26 @@ function App() {
       window.location.pathname = "/login";
     });
   };
+
   return (
     <Router>
       <nav>
         <Link to="/">Home</Link>
-        <Link to="/create_post">Create Post</Link>
 
         {!isAuth ? (
           <Link to="/login">Login</Link>
         ) : (
-          <button onClick={signOutUser}> Log Out</button>
+          <>
+            <Link to="/create_post">Create Post</Link>
+            <button onClick={signOutUser}> Log Out</button>
+          </>
         )}
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login setIsAuth={setIsAuth} />} />
         {/* //passing state as prop to login page */}
-        <Route path="/create_post" element={<CreatePost />} />
+        <Route path="/create_post" element={<CreatePost isAuth={isAuth} />} />
       </Routes>
     </Router>
   );
