a5dfdc87801b81dc15d062d7a429d844e4b8b431
@ahmedyazid.mejri <ahmedyazid.mejri@esprit.tn>
2022-01-01 11:46:12 +0100

    Setting Pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
-
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import CreatePost from "./pages/CreatePost";
 function App() {
-  return <div className="App">react app</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/create_post" element={<CreatePost />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ client/src/pages/CreatePost.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CreatePost = () => {
+  return <div>post</div>;
+};
+
+export default CreatePost;
